5c121aacb2f8a90394baf281d6fa87dde3e78483
Hugo Gouzy <110604228+gotin23@users.noreply.github.com>
2023-03-14 18:43:27 +0100

    first commit

---
+++ assets/script.js
@@ -0,0 +1,102 @@
+const slides = [
+    {
+        image: "slide1.jpg",
+        tagLine:
+            "Impressions tous formats <span>en boutique et en ligne</span>",
+    },
+    {
+        image: "slide2.jpg",
+        tagLine:
+            "Tirages haute définition grand format <span>pour vos bureaux et events</span>",
+    },
+    {
+        image: "slide3.jpg",
+        tagLine: "Grand choix de couleurs <span>de CMJN aux pantones</span>",
+    },
+    {
+        image: "slide4.png",
+        tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
+    },
+];
+
+
+// const previousSlide = document.querySelector(".arrow_left");
+// const nextSlide = document.querySelector(".arrow_right");
+
+const slideActive = document.querySelector(".banner-img");
+const arrows = document.querySelectorAll(".arrow");
+const infoSlide = document.querySelector(".info-slide");
+const dots = document.querySelector(".dots");
+const slideNb = slides.length;
+
+
+// Creation des dots
+for (let i = 0; i < slideNb; i++) {
+    const dotContainer = document.createElement("div");
+    dotContainer.className = "dot";
+    dots.appendChild(dotContainer);
+}
+
+const dotsList = document.querySelectorAll(".dot");
+dotsList[0].classList.add("dot_selected");
+// previousSlide.addEventListener("click", changeSlide);
+// nextSlide.addEventListener("click", changeSlide);
+
+
+arrows.forEach((arrow) => arrow.addEventListener("click", changeSlide ));
+
+let count = 0;
+
+function changeSlide(e) {
+    const rightArrow = count === slideNb - 1 ? 0 : count + 1;
+    const leftArrow = count === 0 ? slideNb - 1 : count + -1;
+
+    if (e.target.classList.contains("arrow_right")) {  
+        slideActive.src ="./assets/images/slideshow/" + slides[rightArrow].image;
+        infoSlide.innerHTML = slides[rightArrow].tagLine;
+        dotsList[count].classList.remove("dot_selected");
+        dotsList[rightArrow].classList.add("dot_selected");
+        count = rightArrow;
+
+    } else if (e.target.classList.contains("arrow_left")) {
+        slideActive.src = "./assets/images/slideshow/" + slides[leftArrow].image;
+        infoSlide.innerHTML = slides[leftArrow].tagLine;
+        dotsList[count].classList.remove("dot_selected");
+        dotsList[leftArrow].classList.add("dot_selected");
+        count = leftArrow;
+    }
+    
+}
+
+// previousSlide.addEventListener("click", () => {
+//   if (count === 0) {
+//     slideActive.src = "./assets/images/slideshow/" + slides[slideNb - 1].image;
+//     infoSlide.innerHTML = slides[slideNb - 1].tagLine;
+//     dotsList[0].classList.remove("dot_selected");
+//     dotsList[slideNb - 1].classList.add("dot_selected");
+//     count = slideNb - 1;
+//     console.log(count);
+//   } else {
+//     slideActive.src = "./assets/images/slideshow/" + slides[count - 1].image;
+//     infoSlide.innerHTML = slides[count - 1].tagLine;
+//     dotsList[count].classList.remove("dot_selected");
+//     dotsList[count - 1].classList.add("dot_selected");
+//     count = count - 1;
+//   }
+// });
+
+// nextSlide.addEventListener("click", () => {
+//   if (count === slideNb - 1) {
+//     slideActive.src = "./assets/images/slideshow/" + slides[0].image;
+//     infoSlide.innerHTML = slides[0].tagLine;
+//     dotsList[slideNb - 1].classList.remove("dot_selected");
+//     dotsList[0].classList.add("dot_selected");
+//     count = 0;
+//   } else {
+//     slideActive.src = "./assets/images/slideshow/" + slides[count + 1].image;
+//     infoSlide.innerHTML = slides[count + 1].tagLine;
+//     dotsList[count].classList.remove("dot_selected");
+//     dotsList[count + 1].classList.add("dot_selected");
+//     count = count + 1;
+//   }
+// });
